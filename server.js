e455a28312838891210ea784517cf91263c09aef
Supresh Colaco <supresh_colaco@persistent.co.in>
2021-03-19 11:13:11 +0530

    deploy issue fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
-app.key=process.env.MASTERKEY
+app.key = process.env.MASTERKEY
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -17,15 +17,17 @@ if (process.env.NODE_ENV === 'development') {
 
 
 require('./db.js')
-require('./routes/transactions')(app)
-require('./routes/users')(app)
-
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
+
+require('./routes/transactions')(app)
+require('./routes/users')(app)
+
+
 
 const PORT = process.env.PORT || 5000;
 //app.get('/',function(req,res){res.send('welcome')})
